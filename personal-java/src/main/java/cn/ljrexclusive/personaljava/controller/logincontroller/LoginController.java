5359c4e203d957dd46bf7996e46b908ef26c46d0
Junery <653233395@qq.com>
2026-01-12 09:43:53 +0800

    fix: 添加前端，后端添加login接口，没实现

---
+++ personal-java/src/main/java/cn/ljrexclusive/personaljava/controller/logincontroller/LoginController.java
@@ -0,0 +1,10 @@
+package cn.ljrexclusive.personaljava.controller.logincontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+}
